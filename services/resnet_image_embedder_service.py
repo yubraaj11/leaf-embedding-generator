4e7eb396995a362f58d5e10c2017efc12d3c4f17
Anish <anishshahkumar@gmail.com>
2024-07-01 21:39:08 +0545

    transform without centercrop

--- services/resnet_image_embedder_service.py
+++ services/resnet_image_embedder_service.py
@@ -53,7 +53,7 @@ class ResNetImageEmbedder:
         image = Image.open(image_path).convert('RGB')
         transform = transforms.Compose([
             transforms.Resize(256),
-            transforms.CenterCrop(224),
+            # transforms.CenterCrop(224),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
